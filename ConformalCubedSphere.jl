e221cb4647c42a48818639d9554010158bf5a73e
Keaton J. Burns <keaton.burns@gmail.com>
2022-12-17 11:10:04 -0500

    Cleanup code and simplify notation a bit

--- ConformalCubedSphere.jl
+++ ConformalCubedSphere.jl
@@ -4,55 +4,80 @@ using Plots
 using LinearAlgebra
 using ForwardDiff
 
-"Gnomic projection from southern face ([-1,1]^2) of unit cube to southern spherical patch."
-function gnomic_projection(x, y; R=1)
-    z = -1
-    r = @. sqrt(x^2 + y^2 + z^2)
-    X = @. R * x / r
-    Y = @. R * y / r
-    Z = @. R * z / r
-    return (X, Y, Z)
+"""
+Gnomic projection from northern face of circumscribed cube to northern spherical patch.
+Input: (cx, cy) in [-1, 1]^2
+Output: (sx, sy, sz) on S^2
+"""
+function gnomic_projection(cx, cy)
+    cz = 1
+    cr = @. sqrt(cx^2 + cy^2 + cz^2)
+    sx = @. cx / cr
+    sy = @. cy / cr
+    sz = @. cz / cr
+    return (sx, sy, sz)
 end
 GP = gnomic_projection
 
-"Stereographic projection of southern spherical patch to C."
-function stereographic_projection(X, Y, Z; R=1)
-    z = @. 2 * R * (X + 1im*Y) / (R - Z)
+function inverse_gnomic_projection(sx, sy, sz)
+    cx = @. sx / sz
+    cy = @. sy / sz
+    return (cx, cy)
+end
+IGP = inverse_gnomic_projection
+
+"""
+Stereographic projection from northern spherical patch to C.
+Input: (sx, sy, sz) on S^2
+Output: z in C
+"""
+function stereographic_projection(sx, sy, sz)
+    z = @. 2 * (sx + 1im*sy) / (1 + sz)
     return z
 end
 SP = stereographic_projection
 
-"Extend by 4-fold rotational symmetry."
+function inverse_stereographic_projection(z)
+    throw("unimplemented")
+    sx = sy = sz = nothing
+    return (sx, sy, sz)
+end
+ISP = inverse_stereographic_projection
+
+"Compose gnomic and stereographic projections."
+project(cx, cy) = SP(GP(cx, cy)...)
+
+"Extend complex vector by 4-fold rotational symmetry."
 extend_C4(z) = vcat(z, im*z, -z, -im*z)
 
-"Extend by degree-4 dihedral symmetry."
-extend_D4(z) = vcat(extend_C4(z), extend_C4(conj(z)))
+"Extend complex vector by degree-4 dihedral symmetry."
+extend_D4(z) = extend_C4(vcat(z, reverse(im*conj(z))))
 
-"Sample boundary exponentially close to corners on inscribed square."
-function sample_boundary_tanh(n, w)
-    s = tanh.(collect(LinRange(0, w, n)))
-    p1 = ones(n)
-    z = SP(GP(p1, s)...)
+"Sample edge exponentially close to corners on circumscribed cube and project to C."
+function sample_edge_tanh(n, w)
+    cx = ones(n)
+    cy = tanh.(LinRange(0, w, n))
+    z = project(cx, cy)
     return z
 end
 
-"Sample boundary linearly on inscribed square."
-function sample_boundary_linear(n)
-    s = collect(LinRange(0, 1, n))
-    p1 = ones(n)
-    z = SP(GP(p1, s)...)
+"Sample edge linearly on circumscribed cube and project to C."
+function sample_edge_linear(n)
+    cx = ones(n)
+    cy = LinRange(0, 1, n)
+    z = project(cx, cy)
     return z
 end
 
 "Samples poles root-exponentially close to corners."
 function sample_poles(n, σ)
-    p = @. exp(-σ * (sqrt(n) - sqrt([1:n;])))
-    zC = SP(GP(1, 1)...)
-    zP = @. zC + cispi(1/4)*p
-    return zP
+    cluster = @. exp(-σ * (sqrt(n) - sqrt([1:n;])))
+    corner = project(1, 1)
+    poles = @. corner + cispi(1/4) * cluster
+    return poles
 end
 
-"f_Runge(z, b) = sum_{j=1}^{nb} b_j z^(1+4(j-1))"
+"f_Runge(z, b) = sum_{j=1}^{n_b} b_j z^(1+4(j-1))"
 function f_Runge(z, b)
     f = zero(z)
     for (j, bj) in enumerate(b)
@@ -62,115 +87,114 @@ function f_Runge(z, b)
     return f
 end
 
-"f_Newman(z, Pz, a) = sum_{j=1}^{na} sum_{k=0}^{3} im a_j (-1)^k / (z - im^k z_j)"
-function f_Newman(z, zP, a)
+"f_Newman(z, p, a) = sum_{j=1}^{n_a} sum_{k=0}^{3} (-1)^k i a_j / (z - i^k p_j)"
+function f_Newman(z, p, a)
     f = zero(z)
-    for (aj, zPj) in zip(a, zP)
+    for (aj, pj) in zip(a, p)
         for k = 0:3
-            fjk = @. im * aj * (-1)^k / (z - im^k * zPj)
+            fjk = @. (-1)^k * im * aj / (z - im^k * pj)
             f += fjk
         end
     end
     return f
 end
 
-"f_ReciprocalLog(z, Pz, a) = sum_{j=1}^{nl} sum_{k=0}^{3} a_j e^{i pi/4} i^k / (z - im^k z_j)"
-function f_ReciprocalLog(z, sP, a)
+"f_ReciprocalLog(z, c, s, a) = ..."
+function f_ReciprocalLog(z, c, s, a)
     f = zero(z)
-    for (aj, sPj) in zip(a, sP)
- for k = 0:3
+    for (aj, sj) in zip(a, s)
+        for k = 0:3
             # this step symmetrizes the branch cut
-             fjk = @. aj * exp(im*pi/4) * (im)^k / (log(-(z/((im)^k * zC) - 1)) + im*pi - sPj)
+             fjk = @. im^k * cispi(1/4) * aj / (log(-(z/(im^k * c) - 1)) + im*pi - sj)
              f += fjk
-             fjk = @. aj * exp(im*pi/4) * (im)^k / (log(-(z/((im)^k * zC) - 1)) - im*pi - sPj)
+             fjk = @. im^k * cispi(1/4) * aj / (log(-(z/(im^k * c) - 1)) - im*pi - sj)
              f += fjk
          end
-  end
+    end
     return f
 end
 
-"f(z) = f_Newman(z) + f_Runge(z)"
-f(z, Pz, a, b) = f_Newman(z, Pz, a) + f_Runge(z, b)
+"Lightning representation."
+f(z, p, a, b) = f_Newman(z, p, a) + f_Runge(z, b)
 
-"fl(z) = f_Newman(z) + f_Runge(z)"
-fLog(z, Ps, a, b) = f_ReciprocalLog(z, Ps, a) + f_Runge(z, b)
+"Log-lightning representation."
+fLog(z, c, s, a, b) = f_ReciprocalLog(z, c, s, a) + f_Runge(z, b)
+
+"""Least-squares fit via autodiff: f'(q0) * q = b"""
+function fit(f, b, q0)
+    # Take Jacobian around initial parameters
+    A = ForwardDiff.jacobian(f, q0)
+    # Compute column norms
+    C = diagm([1/norm(A[:,j]) for j in axes(A, 2)])
+    # Fit
+    q = C * ((A*C) \ b)
+    # Check error
+    e = A * q - b
+    @printf "fitting error: %.2e" maximum(abs.(e))
+    return q
+end
+
 
 # Parameters
 ns = 300  # number of boundary samples per side
 na = 85  # number of poles per corner
-nb = 20  # number of polynomial terms
+nb = 10  # number of polynomial terms
 w = 17  # boundary sampling tanh width
 σ = 4  # pole compaction
 resample = 10  # resampling ratio for testing
 
-Ps = LinRange(-5,20,na)
-
-# Compute and plot boundary samples
-zE = sample_boundary_tanh(ns, w)
-zC = SP(GP(1, 1)...)
-@printf "epsilon side: %.2e \n" minimum(abs.(zE .- zC))
-plot(extend_D4(zE), aspect_ratio=1, seriestype=:scatter)
+# Compute and plot edge samples
+z = sample_edge_tanh(ns, w)
+c = project(1, 1)
+@printf "epsilon edge: %.2e \n" minimum(abs.(z .- c))
+plot(legend=false, grid=false)
+scatter!(extend_D4(z), aspect_ratio=1, msw=0, ms=2)
 
 # Compute and plot poles
-zP = sample_poles(na, σ)
-@printf "epsilon pole: %.2e \n" minimum(abs.(zP .- zC))
-plot!(extend_C4(zP), aspect_ratio=1, seriestype=:scatter)
+p = sample_poles(na, σ)
+@printf "epsilon pole: %.2e \n" minimum(abs.(p .- c))
+scatter!(extend_C4(p), aspect_ratio=1, msw=0, ms=2)
 
-# Least squares fit: Re(fE) = 1
-Re_fq(q) = real(f(zE, zP, q[1:na], q[na+1:end]))
-A = ForwardDiff.jacobian(Re_fq, zeros(na+nb))
-B = ones(ns)
-C = diagm([1/norm(A[:,j]) for j in axes(A, 2)])  # Normalize columns
-Q = C * ((A*C) \ B)
-E = A * Q - B
-@printf "fitting error: %.2e \n" maximum(abs.(E))
+# Least squares fit: Re(f(zE)) = 1
+Re_f(q) = real(f(z, p, q[1:na], q[na+1:end]))
+q = fit(Re_f, ones(ns), zeros(na+nb))
+println(" (f)")
+a = q[1:na]
+b = q[na+1:end]
 
-# Reciprocal log least squares fit: Re(fE) = 1
-Re_fq_Log(q) = real(fLog(zE, Ps, q[1:na], q[na+1:end]))
-ALog = ForwardDiff.jacobian(Re_fq_Log, zeros(na+nb))
-BLog = ones(ns)
-CLog = diagm([1/norm(ALog[:,j]) for j in axes(ALog, 2)])  # Normalize columns
-QLog = CLog * ((ALog*CLog) \ BLog)
-ELog = ALog * QLog - BLog
-@printf "log fitting error: %.2e \n" maximum(abs.(ELog))
-
-# Extract fitted parameters
-a = Q[1:na]
-b = Q[na+1:end]
-
-# Extract fitted parameters for reciprocal log
-aLog = QLog[1:na]
-bLog = QLog[na+1:end]
+s = LinRange(-5,20,na)
+Re_fLog(q) = real(fLog(z, c, s, q[1:na], q[na+1:end]))
+qLog = fit(Re_fLog, ones(ns), zeros(na+nb))
+println(" (fLog)")
+aLog = qLog[1:na]
+bLog = qLog[na+1:end]
 
 # Resample and check error
-ns_rs = resample * ns
-zE_rs = vcat(sample_boundary_linear(ns_rs)...)
-fE_rs = f(zE_rs, zP, a, b)
-fE_rs_Log = fLog(zE_rs, Ps, aLog, bLog)
-EE = real(fE_rs) .- 1
-EELog = real(fE_rs_Log) .- 1
-@printf "resampled error: %.2e \n" maximum(abs.(EE))
-@printf "log resampled error: %.2e \n" maximum(abs.(EELog))
+z_r = vcat(sample_edge_linear(resample * ns)...)
+f_r = f(z_r, p, a, b)
+fLog_r = fLog(z_r, c, s, aLog, bLog)
+@printf "resampling error: %.2e (f)\n" maximum(abs.(real(f_r) .- 1))
+@printf "resampling error: %.2e (fLog)\n" maximum(abs.(real(fLog_r) .- 1))
+scatter!(3 .+ extend_D4(f_r), msw=0, ms=2)
+scatter!(6 .+ extend_D4(fLog_r), msw=0, ms=2)
 
-# plot!(4 .+ extend_D4(fE_rs), seriestype=:scatter)
-# plot!(8 .+ extend_D4(fE_rs_Log), seriestype=:scatter)
-
-x = range(-1, 1, length=20)'
-y = range(-1, 1, length=20)
-z = SP(GP(x, y)...)
-plot!(legend=false)
+# Plot grids
+cx = range(-1, 1, length=20)'
+cy = range(-1, 1, length=20)
+cz = cx .+ im*cy
+z = project(cx, cy)
 for i in axes(z, 1)
     zi = z[i,:]
+    plot!(3im .+ cz[i,:], color="blue", alpha=0.25)
     plot!(3im .+ zi, color="black")
-    plot!(3 .+ 3im .+ f(zi, zP, a, b), color="black")
-    plot!(6 .+ 3im .+ fLog(zi, Ps, aLog, bLog), color="black")
+    plot!(3 .+ 3im .+ f(zi, p, a, b), color="black")
+    plot!(6 .+ 3im .+ fLog(zi, c, s, aLog, bLog), color="black")
 end
 for i in axes(z, 2)
     zi = z[:,i]
+    plot!(3im .+ cz[:,i], color="blue", alpha=0.25)
     plot!(3im .+ zi, color="black")
-    plot!(3 .+ 3im .+ f(zi, zP, a, b), color="black")
-    plot!(6 .+ 3im .+ fLog(zi, Ps, aLog, bLog), color="black")
+    plot!(3 .+ 3im .+ f(zi, p, a, b), color="black")
+    plot!(6 .+ 3im .+ fLog(zi, c, s, aLog, bLog), color="black")
 end
 plot!()
-plot!(3 .+ extend_D4(fE_rs), seriestype=:scatter)
-plot!(6 .+ extend_D4(fE_rs_Log), seriestype=:scatter)
